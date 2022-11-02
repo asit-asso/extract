29705e68d83dd3643f5035d0097b5f8ac7b25a19
Yves Grasset <ygr@arxit.com>
2022-11-02 16:46:26 +0100

    #229 - Documenter l'utilisation de clientguid et organismguid

--- extract/src/main/resources/static/js/extract.js
+++ extract/src/main/resources/static/js/extract.js
@@ -7,6 +7,13 @@
 
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
+    $.fn.popover.Constructor.Default.whiteList.table = [];
+    $.fn.popover.Constructor.Default.whiteList.tr = [];
+    $.fn.popover.Constructor.Default.whiteList.td = [];
+    $.fn.popover.Constructor.Default.whiteList.th = [];
+    $.fn.popover.Constructor.Default.whiteList.div = [];
+    $.fn.popover.Constructor.Default.whiteList.tbody = [];
+    $.fn.popover.Constructor.Default.whiteList.thead = [];
 });
 
 
