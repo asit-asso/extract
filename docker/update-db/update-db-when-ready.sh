21d96c89cc285a8610a55360c5f3e75ebed3860c
Yves Grasset <ygr@arxit.com>
2023-09-21 16:54:27 +0200

    GitHub Actions - Attente de l'application

--- docker/update-db/update-db-when-ready.sh
+++ docker/update-db/update-db-when-ready.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # The web application creates the database, wait for the app to be up, so DB is ready
-#echo "Waiting for EXTRACT to be up..."
-#/wait-for-web.sh http://tomcat:8080/extract/login 100
+echo "Waiting for EXTRACT to be up..."
+/wait-for-web.sh http://tomcat:8080/extract/login 100
 echo "Updating database schema..."
 psql --host=$PGHOST --username=$PGUSER --dbname=$PGDB < /update_db.sql
 psql --host=$PGHOST --username=$PGUSER --dbname=$PGDB < /create_test_data.sql
